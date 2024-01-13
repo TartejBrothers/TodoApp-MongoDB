2e82e04421b7768c651deec3b6b42a8ae03253d8
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2024-01-13 16:28:48 +0530

    Updated backend

--- API/index.js
+++ API/index.js
@@ -7,7 +7,7 @@ app.use(Express.json());
 const port = 5038;
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
@@ -16,8 +16,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const connectString =
-  "mongodb+srv://@todoapp.wrcgueg.mongodb.net/tododb?retryWrites=true&w=majority";
+const connectString = "mongodbstring";
 
 const schema = new mongoose.Schema({
   id: Number,
